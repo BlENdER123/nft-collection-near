8f58c1292adb96c479dd936a5097b2539d9e88c3
Anton Zadorozniy <zadoroshniy_anton_2003@mail.ru>
2022-06-06 18:42:26 +0000

    Add new file

---
+++ scripts/deploy.staging.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SERVER_IP=$SERVER_IP
+SERVER_FOLDER="test.never.defispace.com"
+
+npm install --force
+npm run build
+apt-get update -y
+apt-get -y install rsync
+
+echo "Deploying to ${SERVER_FOLDER}"
+ssh gitlab@${SERVER_IP} "rm -rf /var/www/${SERVER_FOLDER}/*"
+
+rsync -avzh build/* gitlab@${SERVER_IP}:/var/www/${SERVER_FOLDER}/
+
+echo "Finished copying the build files"
